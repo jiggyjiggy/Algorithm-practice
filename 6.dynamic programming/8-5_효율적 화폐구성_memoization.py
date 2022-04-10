21e713476b700f18b3433a04790b8800d15f0773
jiggy <jiggyjiggy0323@gmail.com>
2022-04-10 21:22:21 +0900

    update

--- 6.dynamic programming/8-5_효율적 화폐구성_memoization.py
+++ 6.dynamic programming/8-5_효율적 화폐구성_memoization.py
@@ -4,10 +4,14 @@ target = []
 for i in range(n):
     target.append(int(input()))
 
-memo = [10001] * m
+memo = [10001] * (m + 1)
 
 memo[0] = 0
 for i in range(n):
-    for j in range(target[i], m):
+    for j in range(target[i], m + 1):
         if memo[j-target[i]] != 10001:
-            memo[j] = min()
+            memo[j] = min(memo[j],memo[j-target[i]] + 1)
+
+if memo[m] == 10001:
+    print(-1)
+else:print(memo[m])
