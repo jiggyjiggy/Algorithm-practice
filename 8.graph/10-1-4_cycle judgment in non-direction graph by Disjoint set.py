3e8b0854d960660de59811d87931f1c7245223c9
jiggy <jiggyjiggy0323@gmail.com>
2022-04-17 08:24:14 +0900

    update

--- 8.graph/10-1-4_cycle judgment in non-direction graph by Disjoint set.py
+++ 8.graph/10-1-4_cycle judgment in non-direction graph by Disjoint set.py
@@ -1,12 +1,3 @@
-v, e = map(int, input().split())
-parent = [0] * (v + 1)
-
-for i in range(1, v + 1):
-    parent[i] = i
-
-cycle = False
-
-
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
@@ -19,6 +10,14 @@ def union_parent(parent, a, b):
         parent[b] = a
     else:
         parent[a] = b
+
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+cycle = False
 
 for i in range(e):
     a, b = map(int, input().split())
