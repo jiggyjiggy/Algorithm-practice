3ece9887fa9794e6b60f9968324ae4d4c3c68dcc
jiggy <jiggyjiggy0323@gmail.com>
2022-05-30 00:28:15 +0900

    update

---
+++ greedy/past exam_greedy/모험가 길드_0_X.py
@@ -0,0 +1,39 @@
+# from collections import deque
+#
+# n = int(input())
+# traveler = list(map(int, input().split()))
+#
+# traveler.sort()
+# traveler = deque(traveler)
+#
+# # 공포도 제일 높은 여행자 고르고 그 수에 맞게 배치
+# all_group = []
+#
+# while traveler:
+#     target = traveler.pop()
+#     group = []
+#     for _ in range(target-1):
+#         group.append(traveler.popleft())
+#     group.append(target)
+#     all_group.append(group)
+#
+# print(len(all_group))
+
+###
+# 해설
+n = int(input())
+data = list(map(int, input().split()))
+
+data.sort()
+
+result = 0
+count = 0
+
+for i in data:
+    count += 1
+    if count >= i:
+        result += 1
+        count = 0
+
+print(result)
+###
