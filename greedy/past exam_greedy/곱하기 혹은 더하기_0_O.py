8604bcac43610de7de45aef9d9b0edfb6e8e5614
jiggy <jiggyjiggy0323@gmail.com>
2022-06-06 14:15:00 +0900

    update

--- greedy/past exam_greedy/곱하기 혹은 더하기_0_O.py
+++ greedy/past exam_greedy/곱하기 혹은 더하기_0_O.py
@@ -1,3 +1,4 @@
+###
 nums = input()
 
 target = []
@@ -15,3 +16,26 @@ for idx, i in enumerate(target[1:]):
         result *= int(i)
 
 print(result)
+###
+
+###
+# 입력예시
+# 02984
+#
+# 출력예시
+# 576
+
+# 모범답안
+data = input()
+
+result = int(data[0])
+
+for i in range(1, len(data)):
+    num = int(data[i])
+    if num <= 1 or result <= 1:
+        result += num
+    else:
+        result *= num
+
+print(result)
+###
