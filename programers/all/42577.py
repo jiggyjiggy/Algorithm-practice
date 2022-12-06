c298fbfb82c607e417aa82a1c9a62932d675c119
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-12-06 19:00:50 +0900

    update

--- programers/all/42577.py
+++ programers/all/42577.py
@@ -15,3 +15,25 @@ def solution(phone_book):
     return answer
 
 
+### 타인 풀이
+def solution(phone_book):
+    phone_book = sorted(phone_book)
+
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
+
+
+### hash 사용
+def solution(phone_book):
+    answer = True
+    hash_map = {phone_number: 1 for phone_number in phone_book}
+
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if (temp in hash_map) and (temp != phone_number):
+                answer = False
+    return answer
